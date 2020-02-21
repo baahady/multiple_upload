29e4bef8375289848bcb95ad63b2fb9a7b048dcf
Bahram Ahady <ba.ahady@gmail.com>
2020-02-21 17:21:21 +0330

    check mime type

--- upload.php
+++ upload.php
@@ -2,6 +2,7 @@
 echo"<pre>";
 print_r($_FILES);
 $count = count($_FILES['image']['name']);
+$canUpload = 1;
 for($i=0;$i<$count;$i++){
 
 	if($i==0){
@@ -12,8 +13,23 @@ for($i=0;$i<$count;$i++){
 		$p="parent";
 	}
 
-	$ext = pathinfo($_FILES['image']['name'][$p], PATHINFO_EXTENSION);
-	$newname = rand().time().".".$ext;
-	move_uploaded_file($_FILES['image']['tmp_name'][$p], "upload/$newname");
+	if($_FILES['image']['name'][$p] !== ""){
+	    $finfo = finfo_open(FILEINFO_MIME_TYPE);
+	    $mime = finfo_file($finfo, $_FILES['image']['tmp_name'][$p]);
+	    finfo_close($finfo);
+	    if($mime!="image/jpeg" && $mime!="application/pdf" && $mime!="application/png"){
+	    	$canUpload = 0;
+	    }
+	    
+	}
+
+	if($canUpload==0){
+		echo"problem";
+	}else{
+		$ext = pathinfo($_FILES['image']['name'][$p], PATHINFO_EXTENSION);
+		$newname = rand().time().".".$ext;
+		move_uploaded_file($_FILES['image']['tmp_name'][$p], "upload/$newname");	
+	}
+
 }
 ?>
